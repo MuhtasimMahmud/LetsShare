f5f1c2aa6ad405d8a290b74512e5cadf38e22c51
MuhtasimMahmud <muhtasim.mzs16@gmail.com>
2022-12-01 01:42:54 +0600

    added dislike button for softwareExp

--- src/main/java/com/application/LetsShare/controller/like_dislikeController.java
+++ src/main/java/com/application/LetsShare/controller/like_dislikeController.java
@@ -1,6 +1,7 @@
 package com.application.LetsShare.controller;
 
 import com.application.LetsShare.models.ApprovedExperiences;
+import com.application.LetsShare.models.dislikeCounter;
 import com.application.LetsShare.models.likeCounter;
 import com.application.LetsShare.repositories.ApprovedExpRepository;
 import com.application.LetsShare.repositories.dislikeCounterRepository;
@@ -27,12 +28,20 @@ public class like_dislikeController {
     @Autowired
     dislikeCounterRepository dislikeCounterRepository;
 
+    // jokhon dislike dibe tokhon like_counter thekeu dlt korte hobe
+    // ar jokhon like dibe tokhon dislike counter thekeu dlt korte hobe
+
+
+
+
 
     // RegisteredUser : like software experiences
 
     @RequestMapping("/registeredUser/softwareExp/likePost/{id}")
     public String likeSoftwareExperience(@PathVariable("id") int id, Model model, Principal principal){
 
+
+        // if the current user not liked this post previously, only then we are giving access to like the post.
 
         likeCounter likedPosts = likeCounterRepository.findByExperienceId(id, principal.getName());
 
@@ -47,10 +56,74 @@ public class like_dislikeController {
             likePost.setExperienceId(id);
 
             likeCounterRepository.save(likePost);
+
+
+            // if this used already disliked this post previously, then here removing the dislike as this user is giving like now.
+
+            dislikeCounter dislikedPosts = dislikeCounterRepository.findByExperienceId(id, principal.getName());
+
+            if(dislikedPosts != null){
+
+                approvedExperience.setTotalDislike(approvedExperience.getTotalDislike() - 1);
+                approvedExpRepository.save(approvedExperience);
+
+//                dislikeCounter dislikePost = new dislikeCounter();
+//                dislikePost.setDislikedBy(principal.getName());
+//                dislikePost.setExperienceId(id);
+
+                dislikeCounterRepository.delete(dislikedPosts);
+            }
+
         }else{
             return "redirect:/registeredUser/SoftwareExperiencePage";
         }
 
+        return "redirect:/registeredUser/SoftwareExperiencePage";
+    }
+
+
+    // RegisterUser : dislike software experience
+
+    @RequestMapping("/registeredUser/softwareExp/dislikePost/{id}")
+    public String dislikeSoftwareExperience(@PathVariable("id") int id, Model model, Principal principal){
+
+        // if the current user not disliked this post previously, only then we are giving access to dislike this post.
+
+        dislikeCounter dislikedPosts = dislikeCounterRepository.findByExperienceId(id, principal.getName());
+
+        if(dislikedPosts == null){
+
+            ApprovedExperiences approvedExperience = approvedExpRepository.findById(id);
+            approvedExperience.setTotalDislike(approvedExperience.getTotalDislike()+1);
+
+            approvedExpRepository.save(approvedExperience);
+
+            dislikeCounter dislikePost = new dislikeCounter();
+            dislikePost.setDislikedBy(principal.getName());
+            dislikePost.setExperienceId(id);
+
+            dislikeCounterRepository.save(dislikePost);
+
+
+            // if this current user already liked this post previously, then here we are removing that like as now the user is giving dislike.
+
+            likeCounter likedPosts = likeCounterRepository.findByExperienceId(id, principal.getName());
+
+            if(likedPosts != null) {
+                approvedExperience.setTotalLike(approvedExperience.getTotalLike() - 1);
+
+                approvedExpRepository.save(approvedExperience);
+
+//                likeCounter likePost = new likeCounter();
+//                likePost.setLikedBy(principal.getName());
+//                likePost.setExperienceId(id);
+
+                likeCounterRepository.delete(likedPosts);
+            }
+
+        }else{
+            return "redirect:/registeredUser/SoftwareExperiencePage";
+        }
 
         return "redirect:/registeredUser/SoftwareExperiencePage";
     }
--- src/main/java/com/application/LetsShare/repositories/dislikeCounterRepository.java
+++ src/main/java/com/application/LetsShare/repositories/dislikeCounterRepository.java
@@ -1,8 +1,14 @@
 package com.application.LetsShare.repositories;
 
 import com.application.LetsShare.models.dislikeCounter;
+import com.application.LetsShare.models.likeCounter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface dislikeCounterRepository extends JpaRepository <dislikeCounter, Integer> {
+
+
+    @Query("select d from dislikeCounter d where d.experienceId = :id AND d.dislikedBy = :dislikedPersonEmail")
+    public dislikeCounter findByExperienceId(int id, String dislikedPersonEmail);
 
 }
