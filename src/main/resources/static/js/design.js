7b56be365fbff37d45456eab94eea916d6d700de
MuhtasimMahmud <muhtasim.mzs16@gmail.com>
2022-12-04 13:50:42 +0600

    updated

--- src/main/resources/static/js/design.js
+++ src/main/resources/static/js/design.js
@@ -5,3 +5,30 @@ function work(expId){
 
     // alert("image click is working");
 }
+
+
+function like(id){
+
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.open("GET", 'http://localhost:8080/registeredUser/likePost/'+id);
+    httpRequest.send();
+
+    httpRequest.onload = function (){
+        document.getElementById("like"+id).innerText = httpRequest.responseText;
+    }
+    console.log("js worked");
+
+}
+
+function dislike(id){
+
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.open("GET", 'http://localhost:8080/registeredUser/dislikePost/'+id);
+    httpRequest.send();
+
+    httpRequest.onload = function (){
+        document.getElementById("dislike"+id).innerText = httpRequest.responseText;
+    }
+    console.log("js worked");
+
+}
